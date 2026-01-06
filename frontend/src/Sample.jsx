ca0ade2b5b340399346cb851e289b9960f2bf3cd
thejas <thejassathyesh@gmail.com>
2026-01-06 12:14:45 +0530

    props and desrtructureing in frontend

---
+++ frontend/src/Sample.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sample ({name,age}){
+  
+    
+  return (
+    <div>
+        <h1>my name is {name}</h1>
+        <h3>age is {age}</h3>
+    </div>
+  )
+}
+
+export default Sample
